16771b355b6d6ba93f97231909f9f35a5cd7722f
Durellg <durellgill@gmail.com>
2017-11-09 12:05:56 -0600

    changing url and html

--- src/pages/select-color/select-color.ts
+++ src/pages/select-color/select-color.ts
@@ -44,8 +44,8 @@ export class SelectColorPage {
 		this.analyzeToast();
 		let color;
 		console.log("analyzing...");
-		//let url = this.fireURL;
-		let url = 'https://firebasestorage.googleapis.com/v0/b/boop-a674c.appspot.com/o/images%2F1510245621.jpg?alt=media&token=fbfdeafa-fea9-4da1-af80-3ec157c436ad';
+		let url = this.fireURL;
+		//let url = 'https://firebasestorage.googleapis.com/v0/b/boop-a674c.appspot.com/o/images%2F1510245621.jpg?alt=media&token=fbfdeafa-fea9-4da1-af80-3ec157c436ad';
 		sightengine("1801151869", "bBS92aZfoXDJKm9Y3p8u").check(['properties']).set_url(url).then(function (result) {
 			//this will return a string of the dominant hex value
 			if(result.status == "success"){
@@ -60,7 +60,7 @@ export class SelectColorPage {
 			console.log("this.selectedColorHex is: ", this.selectedColorHex);
 		}).catch(function (err) {
 			console.log(err);
-			
+
 		});
 	}
 
