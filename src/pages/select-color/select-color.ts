2d539bfe8d3ca7ce6b391f694e72d011cd6fb895
Omar Shanti <theomarshanti@gmail.com>
2017-11-10 22:24:21 -0600

    Border for white

--- src/pages/select-color/select-color.ts
+++ src/pages/select-color/select-color.ts
@@ -45,8 +45,8 @@ export class SelectColorPage {
 
 	analyze() {
 		let color;
-		let url = this.fireURL;
-		//let url = 'https://firebasestorage.googleapis.com/v0/b/boop-a674c.appspot.com/o/images%2F1510245621.jpg?alt=media&token=fbfdeafa-fea9-4da1-af80-3ec157c436ad';
+		// let url = this.fireURL;
+		let url = 'https://firebasestorage.googleapis.com/v0/b/boop-a674c.appspot.com/o/images%2F1510245621.jpg?alt=media&token=fbfdeafa-fea9-4da1-af80-3ec157c436ad';
 		sightengine("1801151869", "bBS92aZfoXDJKm9Y3p8u").check(['properties']).set_url(url).then(function (result) {
 			//this will return a string of the dominant hex value
 			if (result.status == "success") {
@@ -57,6 +57,9 @@ export class SelectColorPage {
 		}).then((work) => {
 			this.dominantColor = work;
 			this.selectedColorHex = this.getNearestColor(work);
+			return this.selectedColorHex;
+		}).then((hex)=>{
+			this.navCtrl.push(DiagnosisPage, { selectedColor: hex, image:this.base64Image });
 		}).catch(function (err) {
 			console.log(err);
 		});
@@ -112,15 +115,17 @@ export class SelectColorPage {
 
 	onClickCamera() {
 		this.flashToast();
-		console.log(this.navCtrl.getActive().component.name);
+		// this.navCtrl.push(DiagnosisPage, { selectedColor: this.selectedColorHex, image:this.base64Image });
+
 	}
 
 	onClickUpload() {
 		this.uploadPicture();
+		// this.navCtrl.push(DiagnosisPage, { selectedColor: this.selectedColorHex, image:this.base64Image });
 	}
 
 	onClick(color) {
-		this.selectedColorHex = (this.selectedColorHex === color.hex) ? null : color.hex;
+		this.navCtrl.push(DiagnosisPage, { selectedColor: color.hex, image:""});	
 	}
 
 	getNearestColor(inp_color) {
