276361b895a8da15c65e934fc6f064c0a3aa44ad
Durellg <durellgill@gmail.com>
2017-11-26 19:24:28 -0600

    added share, call, and restart feature to diagnosis page. Not stlyed completely yet

--- src/pages/diagnosis/diagnosis.ts
+++ src/pages/diagnosis/diagnosis.ts
@@ -3,6 +3,8 @@ import { Nav, NavController, NavParams } from 'ionic-angular';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireDatabaseModule, AngularFireDatabase,AngularFireObject } from 'angularfire2/database';
 import * as _ from 'lodash';
+import { SocialSharing } from '@ionic-native/social-sharing';
+
 
 @Component({
   selector: 'page-diagnosis',
@@ -17,14 +19,49 @@ export class DiagnosisPage {
 	text: any;
 	imageSRC: any;
 
-	constructor(public navCtrl: NavController, public navParams: NavParams, public db: AngularFireDatabase) {
+	constructor(public navCtrl: NavController, public navParams: NavParams, public db: AngularFireDatabase, private socialSharing: SocialSharing) {
 		this.selectedColorHex = navParams.get("selectedColor");
 		this.imageSRC = navParams.get("image"); 
 		console.log(this.imageSRC);
 		
 		db.list<any>('/Texts/'+this.selectedColorHex.substring(1)).valueChanges().subscribe(_rawdata=>
 		{
+			
 			[this.selectedColorName, this.severity, this.text] = _rawdata;
 		});
 	}
+
+	shareMe(){
+		var options = {
+			message: 'share this', // not supported on some apps (Facebook, Instagram)
+			subject: 'the subject', // fi. for email
+			//files: ['', ''], // an array of filenames either locally or remotely
+			//url: 'https://www.website.com/foo/#bar?a=b',
+			chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
+		  } 
+
+		this.socialSharing.shareWithOptions(options);
+	}
+
+	backToRoot(){
+		this.navCtrl.popToRoot();
+	}
+
+
+	 onSuccess(result) {
+		console.log("Share completed? " + result.completed); // On Android apps mostly return false even while it's true
+		console.log("Shared to app: " + result.app); // On Android result.app is currently empty. On iOS it's empty when sharing is cancelled (result.completed=false)
+	  }
+	  
+	  onError(msg) {
+		console.log("Sharing failed with message: " + msg);
+	  }
+
+	 options = {
+		message: 'share this', // not supported on some apps (Facebook, Instagram)
+		subject: 'the subject', // fi. for email
+		files: ['', ''], // an array of filenames either locally or remotely
+		url: '',
+		chooserTitle: 'Pick an app' // Android only, you can override the default share sheet title
+	  }
 }
